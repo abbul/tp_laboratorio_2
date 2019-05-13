177e19b8c3cf2f1da7cca6a9849c1a1b87f26e9a
Astroberto Abbul Rodriguez Olivares <astroberto.rodriguez@ibm.com>
2019-05-13 15:49:10 -0300

    __TERMINADO__ Solo falta mejor el codigo

---
+++ tp_2/TP-02-Cascara/TP-02/Entidades/Changuito.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public class Changuito
+    {
+        private List<Producto> productos;
+        private int espacioDisponible;
+        public enum ETipo
+        {
+            Dulce, Leche, Snacks, Todos
+        }
+
+        #region "Constructores"
+        private Changuito()
+        {
+            productos = new List<Producto>();
+        }
+        public Changuito(int espacioDisponible) : this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro el Changuito y TODOS los Productos
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos predicatede ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public static string Mostrar(Changuito c, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<Producto> varios = ( Convert.ToString(tipo) == "Todos") ? c.productos : c.productos.FindAll(x => x.GetType().Name == Convert.ToString(tipo));
+            
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} posibles", c.productos.Count, c.espacioDisponible);
+            sb.AppendLine("");
+
+            foreach (Producto item in varios )
+            {
+                sb.AppendLine(item.Mostrar());
+            }
+
+            return Convert.ToString(sb);
+
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Changuito operator +(Changuito c, Producto p)
+        {
+            if (c.productos.Count < c.espacioDisponible )
+            {
+                foreach (Producto item in c.productos)
+                {
+                    if (item == p)
+                    {
+                        return c;
+                    }
+
+                }
+
+                c.productos.Add(p);
+            }
+            
+
+            return c;
+        }
+
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Changuito operator -(Changuito c, Producto p)
+        {
+            foreach (Producto item in c.productos.ToList() )
+            {
+                if (item == p)
+                {
+                    c.productos.Remove(p);
+                    return c;
+                }   
+            }
+
+            return c;
+        }
+        #endregion
+    }
+}
---
+++ tp_2/TP-02-Cascara/TP-02/Entidades/Leche.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entidades_2018
+{
+    public class Leche : Producto
+    {
+        private ETipo tipo;
+
+        public enum ETipo { Entera, Descremada }
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+
+        public Leche(EMarca marca, string codigoDeBarras, ConsoleColor color, ETipo tipo= ETipo.Entera)
+            : base(marca, codigoDeBarras, color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS: " + CantidadCalorias + " " + "TIPO: " + tipo);
+            sb.AppendLine(" ");
+            sb.AppendLine("---------------------");
+
+            return Convert.ToString(sb);
+        }
+    }
+}
