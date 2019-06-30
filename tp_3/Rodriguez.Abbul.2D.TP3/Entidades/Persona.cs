eaac46b9fda4039d7c9d0951ad3689ff62947100
Astroberto Abbul Rodriguez Olivares <astroberto.rodriguez@ibm.com>
2019-06-30 12:46:08 -0300

    comienzo de tp3

---
+++ tp_3/Rodriguez.Abbul.2D.TP3/Entidades/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private string nacionalidad;
+        private int dni;
+
+    }
+}
