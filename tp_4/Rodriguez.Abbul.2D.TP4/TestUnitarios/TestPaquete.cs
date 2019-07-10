d6f7cabec35bee9900a7928b791d11381e3082ae
Astroberto Abbul Rodriguez Olivares <astroberto.rodriguez@ibm.com>
2019-07-10 01:34:30 -0300

    parte de tp_4

---
+++ tp_4/Rodriguez.Abbul.2D.TP4/TestUnitarios/TestPaquete.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class TestPaquete
+    {
+        [TestMethod]
+        public void ValidaDistintosTrakingID()
+        {
+            Paquete pUno = new Paquete("Av. Corrientes 8541", "123-456-789");
+            Paquete pDos = new Paquete("Calle 25 de Mayo 32", "123-456-789");
+            Paquete pTres = new Paquete("Ruta 248 Km 233443", "113-456-789");
+
+            bool resultado_1 = (pUno == pDos);
+            Assert.AreEqual(true, resultado_1);
+
+            bool resultado_2 = (pUno != pDos);
+            Assert.AreEqual(false, resultado_2);
+
+            bool resultado_3 = (pUno == pTres);
+            Assert.AreEqual(false, resultado_3);
+
+            bool resultado_4 = (pUno != pTres);
+            Assert.AreEqual(true, resultado_4);
+
+        }
+    }
+}
